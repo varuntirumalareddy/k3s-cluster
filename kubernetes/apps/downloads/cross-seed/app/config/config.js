799d464882c46dbf9cd5ebaf47175861e8fbddad
varuntirumala1 <varun.tirumala@gmail.com>
2023-10-11 12:10:51 -0400

    fix links

--- kubernetes/apps/downloads/cross-seed/app/config/config.js
+++ kubernetes/apps/downloads/cross-seed/app/config/config.js
@@ -1,20 +1,20 @@
 module.exports = {
   delay: 20,
-  qbittorrentUrl: "http://qbittorrent.downloads.svc.cluster.local",
+  qbittorrentUrl: "http://qbittorrent.downloads.svc.cluster.local:8080",
   torznab: [
-    "http://prowlarr.downloads.svc.cluster.local/1/api?apikey={{ .PROWLARR__API_KEY }}",  // fl
-    "http://prowlarr.downloads.svc.cluster.local/2/api?apikey={{ .PROWLARR__API_KEY }}",  // avz
-    "http://prowlarr.downloads.svc.cluster.local/8/api?apikey={{ .PROWLARR__API_KEY }}",  // ptp
-    "http://prowlarr.downloads.svc.cluster.local/11/api?apikey={{ .PROWLARR__API_KEY }}", // btn
-    "http://prowlarr.downloads.svc.cluster.local/20/api?apikey={{ .PROWLARR__API_KEY }}", // tl
-    "http://prowlarr.downloads.svc.cluster.local/21/api?apikey={{ .PROWLARR__API_KEY }}", // blu
-    "http://prowlarr.downloads.svc.cluster.local/26/api?apikey={{ .PROWLARR__API_KEY }}", // mtv
-    "http://prowlarr.downloads.svc.cluster.local/40/api?apikey={{ .PROWLARR__API_KEY }}", // uhdb
-    "http://prowlarr.downloads.svc.cluster.local/42/api?apikey={{ .PROWLARR__API_KEY }}", // phd
-    "http://prowlarr.downloads.svc.cluster.local/45/api?apikey={{ .PROWLARR__API_KEY }}", // bhd
-    "http://prowlarr.downloads.svc.cluster.local/47/api?apikey={{ .PROWLARR__API_KEY }}", // ar
-    "http://prowlarr.downloads.svc.cluster.local/48/api?apikey={{ .PROWLARR__API_KEY }}", // ant
-    "http://prowlarr.downloads.svc.cluster.local/49/api?apikey={{ .PROWLARR__API_KEY }}", // athr
+    "http://prowlarr.downloads.svc.cluster.local:9696/1/api?apikey={{ .PROWLARR__API_KEY }}",  // fl
+    "http://prowlarr.downloads.svc.cluster.local:9696/2/api?apikey={{ .PROWLARR__API_KEY }}",  // avz
+    "http://prowlarr.downloads.svc.cluster.local:9696/8/api?apikey={{ .PROWLARR__API_KEY }}",  // ptp
+    "http://prowlarr.downloads.svc.cluster.local:9696/11/api?apikey={{ .PROWLARR__API_KEY }}", // btn
+    "http://prowlarr.downloads.svc.cluster.local:9696/20/api?apikey={{ .PROWLARR__API_KEY }}", // tl
+    "http://prowlarr.downloads.svc.cluster.local:9696/21/api?apikey={{ .PROWLARR__API_KEY }}", // blu
+    "http://prowlarr.downloads.svc.cluster.local:9696/26/api?apikey={{ .PROWLARR__API_KEY }}", // mtv
+    "http://prowlarr.downloads.svc.cluster.local:9696/40/api?apikey={{ .PROWLARR__API_KEY }}", // uhdb
+    "http://prowlarr.downloads.svc.cluster.local:9696/42/api?apikey={{ .PROWLARR__API_KEY }}", // phd
+    "http://prowlarr.downloads.svc.cluster.local:9696/45/api?apikey={{ .PROWLARR__API_KEY }}", // bhd
+    "http://prowlarr.downloads.svc.cluster.local:9696/47/api?apikey={{ .PROWLARR__API_KEY }}", // ar
+    "http://prowlarr.downloads.svc.cluster.local:9696/48/api?apikey={{ .PROWLARR__API_KEY }}", // ant
+    "http://prowlarr.downloads.svc.cluster.local:9696/49/api?apikey={{ .PROWLARR__API_KEY }}", // athr
   ],
   action: "inject",
   includeEpisodes: true,
